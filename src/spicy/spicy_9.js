bf99b460f9600bd6cc4bdf52abb6046bbc059843
Peter Romero <peter@Peters-MacBook-Pro-2.local>
2021-12-04 00:47:47 -0500

    half of spicy

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -15,7 +15,12 @@
  *                from calling the function
  */
 export const repeat = (fn, n, ...params) => {
+    var arr = [];
+    for (var i = 0; i < n; i++) {
+        arr[i] = fn(...params);
+    }
 
+    return arr;
 };
 
 
@@ -24,7 +29,7 @@ export const repeat = (fn, n, ...params) => {
  *   10 times.
  */
 export const repeatDemo = () => {
-
+    repeat(console.log, 10, "Hello, world!");
 };
 
 
@@ -41,7 +46,9 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-
+    return (num2) => {
+        return num1 * num2;
+    };
 };
 
 
@@ -49,7 +56,9 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = (num) => {
+    return num * 10;
+};
 
 
 /**
@@ -57,7 +66,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
@@ -85,7 +94,14 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    var length = arr.length;
+    for (var i = 0; i < length; i += 2) {
+        if (test(arr[i])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 
